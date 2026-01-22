039059fcee47b8b0a37cc53ed24d78e69e4c70ce
Noah C. <caelantandrews@outlook.com>
2026-01-22 23:41:37 +0000

    Preload Google fonts

--- src/components/RoadConditions.jsx
+++ src/components/RoadConditions.jsx
@@ -3,7 +3,7 @@ import "./styles/RoadConditions.css"
 export default function RoadConditions() {
     return (
         <>
-            <iframe className="embed" src="https://traveler.modot.org/map/" frameborder="0"></iframe>
+            <iframe className="embed" src="https://traveler.modot.org/map/" loading="lazy" frameborder="0"></iframe>
         </>
     )
 }
